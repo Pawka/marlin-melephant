3cb40abbe4c6464d5d49c6ba78480c5f305e6c53
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2020-05-22 22:53:28 +0300

    Calibrated printing surface dimenssions: 180x200x175

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -880,12 +880,12 @@
 #define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 2
-#define Y_MIN_POS -20
+#define X_MIN_POS 0
+#define Y_MIN_POS -15
 #define Z_MIN_POS 0
-#define X_MAX_POS X_MIN_POS + X_BED_SIZE
-#define Y_MAX_POS Y_MIN_POS + Y_BED_SIZE
-#define Z_MAX_POS 160
+#define X_MAX_POS X_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE
+#define Z_MAX_POS 175
 
 /**
  * Software Endstops
